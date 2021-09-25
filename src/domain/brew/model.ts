36a3b191476f798be566e2ec8e2b8dc71ee716ad
w00 <w00@Macintosh.local>
2021-09-26 02:11:57 +0900

    added missing location to brew model

--- src/domain/brew/model.ts
+++ src/domain/brew/model.ts
@@ -6,12 +6,16 @@ export type Brew = {
   name: string;
   producerId: string;
   batch: string;
+  location: [number];
 };
 
 const brewSchema: Schema = new Schema({
   name: String,
   producerId: String,
   batch: Schema.Types.ObjectId,
+  location: [Number],
+}).index({
+  location: "2dsphere",
 });
 
 export const BrewModel = model<Brew & Document>("Brew", brewSchema);
